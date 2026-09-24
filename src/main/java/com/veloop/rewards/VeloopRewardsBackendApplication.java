9ac56c9686797642ec03c52dafcd2ed76a3ada1b
sonu kumar <gautamrocky909621@gmail.com>
2026-09-24 22:01:13 +0530

    at the moment to begin the start the project

---
+++ src/main/java/com/veloop/rewards/VeloopRewardsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veloop.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloopRewardsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloopRewardsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veloop/rewards/VeloopRewardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veloop.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloopRewardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
